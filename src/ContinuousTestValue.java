ba766eee4f028cfd584f2ebf6ed5eadf42389142
Barbara Schwarzwald <broeckschen@googlemail.com>
2012-11-14 21:57:07 +0100

    TestValue --> important edit

--- src/ContinuousTestValue.java
+++ src/ContinuousTestValue.java
@@ -2,28 +2,55 @@
 public class ContinuousTestValue implements TestValue {
 
 	private	Attribute attribute;
-	private String splitinfo;
+	private SplitType splitType;
+	private double splitValue;
 	
 
 	public Attribute getAttribute(){
 		return attribute;
 	}
 	
-	public String getSplitInfo(){
-		return new String(splitinfo);
+	public SplitType getSplitType(){
+		return splitType;
 	}
 	
 	public void setAttribute(Attribute a){
 		attribute = a;
 	}
 	
-	public void setSplitInfo(String info){
-		this.splitinfo = info;
+	public void setSplitType(SplitType type){
+		this.splitType = type;
 	}
-	@Override
-	public String printTest() {
-		// TODO Auto-generated method stub
-		return Integer.toString(attribute.getId())+" "+ splitinfo;
+	
+	public double getSplitValue() {
+		return splitValue;
 	}
 
+	public void setSplitValue(double splitValue) {
+		this.splitValue = splitValue;
+	}
+
+	@Override
+	public String printTest() {
+		String splitinfo;
+		switch(splitType){
+		case LESS:
+			splitinfo = "<";
+			break;
+		case LESSOREQUAL:
+			splitinfo = "<=";
+			break;
+		case MOREOREQUAL:
+			splitinfo = ">=";
+			break;
+		case MORE:
+			splitinfo = ">";
+			break;
+		default:
+			splitinfo ="";
+		}
+		return Integer.toString(attribute.getId())+" "+ splitinfo + " " + splitValue;
+	}
+
+	public enum SplitType{LESS, LESSOREQUAL, MOREOREQUAL, MORE};
 }
