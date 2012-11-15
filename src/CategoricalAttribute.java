0ef645d1a696c363bb0cc57e4b9aa5586677f141
mo-fu <mo.fu@rocketmail.com>
2012-11-15 01:25:13 +0100

    Update src/CategoricalAttribute.java

--- src/CategoricalAttribute.java
+++ src/CategoricalAttribute.java
@@ -1,28 +1,52 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;;
 
 
 public class CategoricalAttribute extends Attribute {
 
 
-	private List<Number> valueSet;
+	private ArrayList<Integer> valueSet;
 	
 	public CategoricalAttribute(int id){
 		super(id);
-		valueSet = new ArrayList<Number>();
+		valueSet = new ArrayList<Integer>();
 	}
 	public boolean isContinuous(){
 		return false;
 	}
 	
-	public List getValueSet(){
+	public List<Integer> getValueSet(){
 		return new ArrayList(valueSet);
 	}
 	
 	public void addValue(Number newValue){
 		if(!valueSet.contains(newValue)){
-			valueSet.add(newValue);
+			valueSet.add(newValue.intValue());
 		}
+	}
+	public double computeEntropy(SampleSet set){
+		double entropy = 0.0;
+		int[] pos=new int[valueSet.size()],neg = new int[valueSet.size()];
+		for(Sample s: set.samples){
+			for(Integer v :valueSet){
+				if(v == s.getValue(this.getId())){
+					if(s.getResult() == true) pos[v] +=1;
+					else neg[v] +=1;
+				}
+			}
+		}
+		//compute Entropy.
+		for(Integer v: valueSet){
+			entropy += ((neg[v]+pos[v])/set.samples.size())
+							*(-(java.lang.Math.log(neg[v]/(neg[v]+pos[v]))*neg[v])
+									/(java.lang.Math.log(2.0)*(neg[v]+pos[v]))
+								-(java.lang.Math.log(pos[v]/(pos[v]+neg[v])*pos[v])
+										/(java.lang.Math.log(2.0)*(neg[v]+pos[v]))
+							)
+						);
+		}
+		return entropy;
 	}
 	
 }
