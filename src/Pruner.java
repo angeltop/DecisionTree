9714ae59147fc0c6601070ecd10cf17ef03ca59a
Barbara Schwarzwald <broeckschen@googlemail.com>
2012-11-16 00:00:08 +0100

    Pruner: Bug-fix, revisited comments
    Fixed small but significant bug (my fault)

--- src/Pruner.java
+++ src/Pruner.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /* 
- * The Pruner class is used to prune a given decision Tree using a new SampleSet
- * The SampleSet should be different from the Sample Set used to create the tree
- * to ensure effectiveness of the pruner
+ * The Pruner class is used to prune a given Decision Tree using a new set of samples.
+ * The samples should be different from the samples used to create the tree to ensure
+ * the effectiveness of the Pruner.
  */
 
 public class Pruner {
 	
 	/*
-	 * The pruner requires the pruningSet and the root node of the tree to prune to get start
+	 * The Pruner requires the pruning samples and the root node of the tree to prune.
 	 * Therefore these are the members and constructor parameters of the Pruner
 	 */
 	SampleSet pruningSet;
@@ -22,7 +22,7 @@ public class Pruner {
 	}
 	
 	/*
-	 * The actual pruning requires some preparation
+	 * The actual pruning requires some preparation:
 	 * First the pruningSet is distributed over the tree according to the node tests,
 	 * and the according test values of the children.
 	 * After that the tree is pruned.
@@ -33,14 +33,14 @@ public class Pruner {
 	}
 	
 	/*
-	 * The division of the samples is done recursively.
+	 * The distribution of the samples is done recursively.
 	 * divideSamples(node, nodeSet) works for any subtree.
 	 */
 	private void divideSamples(Node node, SampleSet nodeSet){
 		/*
 		 * The first step is to assign the given set to the top node of the subtree.
-		 * If it is a leaf, we're done, else we have to divide the samples into the
-		 * correct sampleSets for the subtrees.
+		 * If it is a leaf, we're done; else we have to divide the samples into the
+		 * correct subsets for the subtrees.
 		 */
 		node.setSampleSet(nodeSet);
 		if(!node.isLeaf()){
@@ -52,7 +52,7 @@ public class Pruner {
 			 * For tests on continuous attributes, the division is always binary.
 			 * Therefore we can divide the set into the set for which the test is true
 			 * and the set for which it's false. This is obviously dependent on the 
-			 * test of the node.
+			 * test of the node, which is always a comparison "<x" due to the construction implementation.
 			 */
 			if(nodeTest instanceof ContinuousTest){
 				
@@ -61,7 +61,6 @@ public class Pruner {
 				SampleSet falseSet = new SampleSet();
 				falseSet.setAtrributes(nodeSet.getAttributes());
 				
-				
 				for(Sample s: nodeSet.getSamples()){
 					if(s.getValue(nodeTest.getTestAttribute().getId()).doubleValue() < ((ContinuousTest) nodeTest).getSplitValue()){
 						trueSet.addSample(s);
@@ -69,13 +68,12 @@ public class Pruner {
 						falseSet.addSample(s);
 					}
 				
-				
 				/*
-				 * After the determination of the true and false set dependent on the test
-				 * those sets are recursively divide among the two subtrees with the children on top.
+				 * After the determination of the true and false set dependent on the node's test
+				 * those sets are recursively distributed among the two subtrees starting with the node's children.
 				 */
 				for(Node child: decisionNode.getChildren()){
-					if (((ContinuousTestValue) child.getTestValue()).value = true){
+					if (((ContinuousTestValue) child.getTestValue()).value == true){
 						divideSamples(child, trueSet);
 					}else{
 						divideSamples(child, falseSet);
@@ -85,8 +83,8 @@ public class Pruner {
 			/*
 			 * For categorical Attributes the distribution is done per child.
 			 * For each child, all samples with the correct attribute value according
-			 * to the testValue of the child are assembled in a new SampleSet.
-			 * Then the sampleset is divided in the subtree with that child on top.
+			 * to the testValue of the child are assembled in a new set of samples.
+			 * Then the derived samples are distributed in the subtree of that child.
 			 */
 			else{
 				CategoricalAttribute decisionAttribute = (CategoricalAttribute) decisionNode.getTest().getTestAttribute();
@@ -95,8 +93,10 @@ public class Pruner {
 					SampleSet childSet = new SampleSet();
 					childSet.setAtrributes(nodeSet.getAttributes());
 				
+					String childValue = ((CategoricalTestValue) decisionNode.getTestValue()).getValue();
+					
 					for(Sample s: nodeSet.getSamples()){
-						if(s.getValue(decisionAttribute.getId()).equals(decisionAttribute.addValue(((CategoricalTestValue) decisionNode.getTestValue()).getValue()))){
+						if(s.getValue(decisionAttribute.getId()).equals(decisionAttribute.addValue(childValue))){
 							childSet.addSample(s);
 						}
 					}
@@ -108,14 +108,15 @@ public class Pruner {
 	}
 	
 	/*
-	 * The pruning of any subtree is done recursively.
-	 * First, all subtrees of the current top node are pruned.
-	 * The resulting subtrees are treated as the new children of the top node.
-	 * Then the success probability of the subtree is compared to the success probability, 
-	 * if the current top node were treated as a leaf.
-	 * If treating it as a leaf would increase the success probability an according new Leaf
-	 * is created and returned as the pruned subtree.
-	 * If we are at a leaf, we simply check if the label fits the pruning set.
+	 * The pruning of any subtree is done recursively:
+	 * If we are at a leaf, we are at the end of the recursion and simply check if the label
+	 * fits the pruning set.
+	 * Else we need to prune all subtrees of the current top node first. The top nodes of the
+	 * resulting subtrees are treated as the new children of the top node.
+	 * Then the success probability of the subtree is compared with the success probability, 
+	 * if the current top node were a leaf instead.
+	 * If treating it as a leaf increases the success probability of the subtree an according
+	 * new Leaf is created and returned as the pruned subtree.
 	 */
 	private Node pruneSubtree(Node node) {
 		if(!node.isLeaf()){
@@ -163,8 +164,8 @@ public class Pruner {
 	 * This function returns the success probability of a node, if it were a leaf.
 	 * The number of + and - classified samples are counted and the probabilities calculated.
 	 * If the node actually is a leaf, the probability of the labeled class is returned.
-	 * If is is not, the bigger probability is returned, since that is the label,
-	 * since the optimal choice would be made for a leaf based on that node.
+	 * If is is not, the bigger probability is returned, since the optimal choice would be made
+	 * for a leaf based on that node.
 	 */
 	public double getNodeSuccessProbability(Node node) {
 		double trueCount=0;
