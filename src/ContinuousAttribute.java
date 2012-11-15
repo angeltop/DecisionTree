06eb28fde593b112c472d7dcbd796738c50d393d
Moritz Fürneisen <user@Maurizio-Perferros-MacBook-Pro.local>
2012-11-15 11:14:02 +0100

    New stuff for continuous attributes. Not finished yet.

--- src/ContinuousAttribute.java
+++ src/ContinuousAttribute.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
 
-public class ContinuousAttribute extends Attribute {
 
+
+public class ContinuousAttribute extends Attribute implements Comparator<Sample> {
+	
+	private double splitValue;
 	
 	
 	public ContinuousAttribute(int id){
@@ -8,9 +14,35 @@ public class ContinuousAttribute extends Attribute {
 	}
 	
 public double computeEntropy(SampleSet set){
-	double entropy = 0.0;	
+	double entropy = 0.0;
+	Integer count = new Integer(0);
+	int pos=0,neg=0,total=0;
+	boolean status = true;
+	Iterator<Sample> it;
+	java.util.Collections.sort(set.getSamples(),this);
+	for(Sample s : set.getSamples()){
+		if(s.getResult())pos++;
+		else neg++;
+	}
+	total=pos+neg;
+	for(Sample s : set.getSamples()){
+		
+	}
 	return entropy;
 	}
+
+public double getSplitValue(){
+	return splitValue;
+}
+
+
+@Override
+public int compare(
+		Sample o1, Sample o2) {
+	double d1 = ((Sample) o1).getValue(this.getId()).doubleValue();
+	double d2 = ((Sample) o2).getValue(this.getId()).doubleValue();
+	return Double.compare(d1, d2);
+}
 
 
 }
